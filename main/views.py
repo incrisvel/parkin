0402a82e23e3a100c3953f85d5829a8507fc72e6
Bia <marcomartins051@gmail.com>
2024-01-24 14:58:54 -0300

    adcionais

--- main/views.py
+++ main/views.py
@@ -4,10 +4,34 @@ def index(request):
     return render(request,'main/index.html')
 
 def cadastro(request):
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    confirme = request.POST.get('confirme')
+    cpf = request.POST.get('cpf')
+    cnpj = request.POST.get('cnpj')
+    data = request.POST.get('data')
+    telefone = request.POST.get('cell')
+    check = 'on'
     if request.method == 'POST':
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirme = request.POST.get('confirme')
+        cpf = request.POST.get('cpf')
+        cnpj = request.POST.get('cnpj')
         data = request.POST.get('data')
-            
-    return render(request, 'main/cadastro.html')
+        telefone = request.POST.get('cell')
+        check = request.POST.get('check')
+        print (email,check)
+
+    context = {
+        'email': email,
+        'senha' : senha,
+        'confirme' : confirme,
+        'cpf' : cpf,
+        'cnpj' : cnpj,
+        'data' : data,
+        'telefone' : telefone,
+        'check' : check
+    }
+
+    return render(request, 'main/cadastro.html', context)
