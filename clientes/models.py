f6bf02af740fce5a780fe648fbc59a107d20b98f
Cristina SJ <sjcristina9@gmail.com>
2024-01-24 10:43:46 -0300

    models-inicio

--- clientes/models.py
+++ clientes/models.py
@@ -9,3 +9,30 @@ class Cliente(models.Model):
     
     def __str__(self):
         return self.nome
+    
+    class Meta:
+        ordering = ['nome']
+        
+        
+class Avaliacao (models.Model):
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='usuario_avaliador', verbose_name='Avaliador')
+    livro = models.ForeignKey(
+        Livro, on_delete=models.CASCADE, related_name='livro_avaliado', verbose_name='Livro avaliado')
+    dissertacao = models.CharField(max_length=1000, null=True, verbose_name='Dissertação')
+    data_envio = models.DateTimeField(auto_now_add=True, verbose_name='Data de envio')
+    nota = models.DecimalField(max_digits=3, decimal_places=1, validators=[
+                               MinValueValidator(0), MaxValueValidator(10)])
+
+    def __str__(self):
+        return f'Avaliação de {self.usuario} para {self.livro.titulo}'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.livro.nota_media
+        
+    class Meta:
+        ordering = ['data_envio']
+        
+        verbose_name ='avaliação de usuários'
+        verbose_name_plural = 'avaliações de usuários'
--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario (models.Model):
+    id = models.AutoField(primary_key=True)
+
+class Feedback (models.Model):
+    nome = models.CharField(max_length=100)
+    mensagem = models.TextField()
+    data_envio = models.DateTimeField(auto_now_add=True)
