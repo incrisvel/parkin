fbf703707ca124c688aad06375a7a83963ce1d92
Bia <marcomartins051@gmail.com>
2024-01-24 16:54:09 -0300

    adcionais

--- main/forms.py
+++ main/forms.py
@@ -1,1 +1,11 @@
 from django import forms
+
+class Cadastro(forms.Form):
+    nome = forms.CharField(label='Nome *', required = True)
+    email = forms.CharField(label='Email *', required = True)
+    senha = forms.CharField(label='Senha *', required = True)
+    confirme = forms.CharField(label='Confirme a senha *', required = True)
+    cpf = forms.CharField(label='CPF', required = True )
+    datadenascimento = forms.CharField(label='Data de nascimento', required = False)
+    telefone = forms.CharField(label='Telefone', required = False)
+    checkbox = forms.BooleanField()
--- main/views.py
+++ main/views.py
@@ -1,37 +1,17 @@
 from django.shortcuts import render
+from .forms import Cadastro
 
 def index(request):
     return render(request,'main/index.html')
 
 def cadastro(request):
-    email = request.POST.get('email')
-    senha = request.POST.get('senha')
-    confirme = request.POST.get('confirme')
-    cpf = request.POST.get('cpf')
-    cnpj = request.POST.get('cnpj')
-    data = request.POST.get('data')
-    telefone = request.POST.get('cell')
-    check = 'on'
     if request.method == 'POST':
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-        confirme = request.POST.get('confirme')
-        cpf = request.POST.get('cpf')
-        cnpj = request.POST.get('cnpj')
-        data = request.POST.get('data')
-        telefone = request.POST.get('cell')
-        check = request.POST.get('check')
-        print (email,check)
-
-    context = {
-        'email': email,
-        'senha' : senha,
-        'confirme' : confirme,
-        'cpf' : cpf,
-        'cnpj' : cnpj,
-        'data' : data,
-        'telefone' : telefone,
-        'check' : check
-    }
+        if form.is_valid():
+            form.save() 
+    else:
+        form = Cadastro()
+        context = {
+        'form' : form
+        }
 
     return render(request, 'main/cadastro.html', context)
