7e13a0f229c96f6eab01c03e1550e141375e5bd4
pcadmin <ryanwds101@gmail.com>
2024-01-25 16:55:46 -0300

    novasatualiza

--- main/forms.py
+++ main/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class Cadastro(forms.Form):
-    nome = forms.CharField(label='Nome *', required = True)
-    email = forms.CharField(label='Email *', required = True)
-    senha = forms.CharField(label='Senha *', required = True)
-    confirme = forms.CharField(label='Confirme a senha *', required = True)
-    cpf = forms.CharField(label='CPF', required = True )
-    datadenascimento = forms.CharField(label='Data de nascimento', required = False)
-    telefone = forms.CharField(label='Telefone', required = False)
-    checkbox = forms.BooleanField()
+    nome = forms.CharField(label='Nome *')
+    email = forms.CharField(label='Email *')
+    senha = forms.CharField(label='Senha *', widget=forms.PasswordInput)
+    confirme = forms.CharField(label='Confirme a senha *', widget=forms.PasswordInput)
+    cpf = forms.CharField(label='CPF *')
+    cnpj = forms.CharField(label='CNPJ *', required= False)
+    datadenascimento = forms.CharField(label='Data de nascimento')
+    telefone = forms.CharField(label='Telefone')
--- main/views.py
+++ main/views.py
@@ -1,17 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import Cadastro
 
 def index(request):
     return render(request,'main/index.html')
 
 def cadastro(request):
+    check = 'on'
+    senha = ''
+    confirme = ''
     if request.method == 'POST':
+        form = Cadastro(request.POST)
+        check = request.POST.get('check')
         if form.is_valid():
-            form.save() 
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            senha = form.cleaned_data['senha']
+            confirme = form.cleaned_data['confirme']
+            cpf = form.cleaned_data['cpf']
+            cnpj = form.cleaned_data['cnpj']
+            data = form.cleaned_data['datadenascimento']
+            telefone = form.cleaned_data['telefone']
+            if senha == confirme:
+                return redirect('save/')
+            else:
+                Cadastro(initial={'nome':nome, 'email': email, 'senha': senha, 'confirme':confirme, 'cpf':cpf, 'data':data, 'telefone':telefone})
     else:
         form = Cadastro()
-        context = {
-        'form' : form
-        }
-
+    context = {
+        'form' : form,
+        'check' : check,
+        'senha' : senha,
+        'confirme' : confirme,
+    }
     return render(request, 'main/cadastro.html', context)
