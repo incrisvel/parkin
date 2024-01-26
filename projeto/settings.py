37347a5b1e1ae0c5b30a09d9c7a2a445aacd7f70
pcadmin <ryanwds101@gmail.com>
2024-01-26 16:05:30 -0300

    Merge Dev sem conflitos

--- projeto/settings.py
+++ projeto/settings.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'empresas'
+    'empresas',
+    'clientes',
 ]
 
 MIDDLEWARE = [
@@ -106,4 +107,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'main.Usuario'
 
