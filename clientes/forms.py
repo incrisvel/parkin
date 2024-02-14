c9f4c1369b997f13a215a1047564d7eae7999976
ryanwds101 <ryanwds101@gmail.com>
2024-02-14 16:55:24 -0300

    CSS - Registro

--- clientes/forms.py
+++ clientes/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Cliente
 
 class Usuario(forms.ModelForm):
-    senha = forms.CharField(widget=forms.PasswordInput)
+    senha = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Senha'}))
     class Meta:
         model = Cliente
         fields = "__all__"
@@ -10,7 +10,9 @@ class Usuario(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.fields['data_nasc'].widget = forms.DateInput(attrs={'type':'date'})
+        self.fields['data_nasc'].widget = forms.DateInput(attrs={'type': 'date', 'placeholder': 'Data de Nascimento'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
+        self.fields['nome'].widget.attrs.update({'placeholder': 'Nome'})
     
     def clean_email(self):  
         email = self.cleaned_data['email']
@@ -18,6 +20,7 @@ class Usuario(forms.ModelForm):
             print("Email já existe")
             raise forms.ValidationError('Email já cadastrado!')
         return email
+    
     def clean_nome(self):   
         nome = self.cleaned_data['nome']
         if Cliente.objects.filter(nome=nome).exists():
--- empresas/forms.py
+++ empresas/forms.py
@@ -2,10 +2,18 @@ from django import forms
 from .models import Estacionamento
 
 class Empresas(forms.ModelForm):
-    senha = forms.CharField(widget=forms.PasswordInput)
+    senha = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Senha'}))
     class Meta:
         model = Estacionamento
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['nome_fantasia'].widget.attrs['placeholder'] = 'Nome Fantasia'
+        self.fields['cnpj'].widget.attrs['placeholder'] = 'CNPJ'
+        self.fields['razao_social'].widget.attrs['placeholder'] = 'Razão Social'
+
 
     def clean_email(self):  
         email = self.cleaned_data['email']
@@ -13,12 +21,14 @@ class Empresas(forms.ModelForm):
             print("Email já existe")
             raise forms.ValidationError('Email já cadastrado!')
         return email
+
     def clean_nome_fantasia(self):  
         nome_fantasia = self.cleaned_data['nome_fantasia']
         if Estacionamento.objects.filter(nome_fantasia=nome_fantasia).exists():
             print("Nome já existe")
             raise forms.ValidationError('Nome já cadastrado!')
         return nome_fantasia
+
     def clean_cnpj(self):  
         cnpj = self.cleaned_data['cnpj']
         if Estacionamento.objects.filter(cnpj=cnpj).exists():
