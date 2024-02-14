443422eff6dc109302e58a1380517fac140dc2b7
ryanwds101 <ryanwds101@gmail.com>
2024-02-14 17:43:43 -0300

    arrumando

--- empresas/forms.py
+++ empresas/forms.py
@@ -1,10 +1,19 @@
 from django import forms
-from .models import Estacionamento
+from .models import PerfilLocal, Endereco
 
 class Empresas(forms.ModelForm):
     senha = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Senha'}))
     class Meta:
-        model = Estacionamento
+        model = PerfilLocal
+        fields = "__all__"
+        widgets = {
+            'hora_abre': forms.TimeInput(attrs={'type': 'time'}),
+            'hora_fecha': forms.TimeInput(attrs={'type': 'time'}),
+            }
+
+class Estacionamento(forms.ModelForm):
+    class Meta:
+        model = Endereco
         fields = "__all__"
 
     def __init__(self, *args, **kwargs):
