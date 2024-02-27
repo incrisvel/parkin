9086ca02a43fec3527551b8dbd86821d055e1c78
Acesso20244 <ryanwds101@gmail.com>
2024-02-27 15:52:09 -0300

    Avaliacao

--- mapa/urls.py
+++ mapa/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from mapa.views import estacionamentos, detalhe_estacionamento
 
 app_name = 'estacionamentos'
 
 urlpatterns = [
     path('', estacionamentos, name='estacionamentos'),
-    path('<int:estacionamento_id>/', detalhe_estacionamento, name='detalhe_estacionamento'),
-
+    path('detalhe_estacionamento/<int:estacionamento_id>/', detalhe_estacionamento, name='detalhe_estacionamento'),
 ]
--- mapa/views.py
+++ mapa/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from empresas.models import PerfilLocal
+from .forms import AvaliacaoForm
+from django.contrib.auth.decorators import login_required
+
+
 
 def estacionamentos(request):
     locais = PerfilLocal.objects.all()
@@ -15,19 +19,25 @@ def estacionamentos(request):
     return render(request, "mapa/estacionamentos.html", {'locais' : locais})
 
 def detalhe_estacionamento(request, estacionamento_id):
+    estacionamento = get_object_or_404(PerfilLocal, pk=estacionamento_id)
+    if request.method == 'POST':
+        form = AvaliacaoForm(request.POST)
+        if form.is_valid():
+            avaliacao = form.save(commit=False)
+            avaliacao.avaliador = request.user.cliente
+            avaliacao.avaliado = estacionamento
+            avaliacao.save()
+            return redirect('estacionamentos:detalhe_estacionamento', estacionamento_id=estacionamento_id)
+    else:
+        form = AvaliacaoForm()
     
-    locais = PerfilLocal.objects.all()
-
     coberto_choice = {True: 'Coberto', False: 'Não coberto'}
     dias_semana = {'1': 'Segunda-feira', '2': 'Terça-feira', '3': 'Quarta-feira', '4': 'Quinta-feira', '5': 'Sexta-feira', '6': 'Sábado', '7': 'Domingo'}
 
+    estacionamento.coberto = coberto_choice[estacionamento.coberto]
 
-    for local in locais:
+    dias_abertos = [dias_semana[dia] for dia in estacionamento.dias_aberto]
+    estacionamento.dias_abertos = dias_abertos
+    
 
-        coberto = coberto_choice[local.coberto]
-        local.coberto = coberto
-        dias_abertos = [dias_semana[dia] for dia in local.dias_aberto]
-        local.dias_abertos = dias_abertos
-
-    estacionamento = get_object_or_404(PerfilLocal, pk=estacionamento_id)
-    return render(request, 'mapa/detalhe_estacionamento.html', {'estacionamento': estacionamento, 'locais': locais})
+    return render(request, 'mapa/detalhe_estacionamento.html', {'estacionamento': estacionamento, 'form': form})
