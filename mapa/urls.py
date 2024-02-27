9f69a2659a44e9ea8405f15e3eef8096231a6622
Acesso20244 <ryanwds101@gmail.com>
2024-02-26 22:29:31 -0300

    Página do estacionamento

--- mapa/urls.py
+++ mapa/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from mapa.views import estacionamentos
+from mapa.views import estacionamentos, detalhe_estacionamento
 
 app_name = 'estacionamentos'
 
 urlpatterns = [
     path('', estacionamentos, name='estacionamentos'),
+    path('<int:estacionamento_id>/', detalhe_estacionamento, name='detalhe_estacionamento'),
+
 ]
--- mapa/views.py
+++ mapa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from empresas.models import PerfilLocal
 
 def estacionamentos(request):
@@ -13,3 +13,21 @@ def estacionamentos(request):
 
 
     return render(request, "mapa/estacionamentos.html", {'locais' : locais})
+
+def detalhe_estacionamento(request, estacionamento_id):
+    
+    locais = PerfilLocal.objects.all()
+
+    coberto_choice = {True: 'Coberto', False: 'Não coberto'}
+    dias_semana = {'1': 'Segunda-feira', '2': 'Terça-feira', '3': 'Quarta-feira', '4': 'Quinta-feira', '5': 'Sexta-feira', '6': 'Sábado', '7': 'Domingo'}
+
+
+    for local in locais:
+
+        coberto = coberto_choice[local.coberto]
+        local.coberto = coberto
+        dias_abertos = [dias_semana[dia] for dia in local.dias_aberto]
+        local.dias_abertos = dias_abertos
+
+    estacionamento = get_object_or_404(PerfilLocal, pk=estacionamento_id)
+    return render(request, 'mapa/detalhe_estacionamento.html', {'estacionamento': estacionamento, 'locais': locais})
